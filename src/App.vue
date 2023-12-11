6d3e1c3d7d509fe58fb58099c316a425fd1439a8
Ana Maria Correa <111539281+AnaC1997@users.noreply.github.com>
2023-12-11 16:48:46 +0100

    Template
    Tecnologie usate: HTML, VueJS.3 e SASS

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script>
+import PageHeader from "./components/PageHeader.vue"
+import PageMain from "./components/PageMain.vue";
+import pageFooter from "./components/PageFooter.vue"
+
+//import axios from 'axios'; //importo Axios in caso di bisogno
+import { store } from "./store.js"
+
+export default {
+	components: {
+    PageHeader,
+    PageMain,
+    pageFooter
+		
+	},
+	data() {
+		return {
+			store
+		}
+	},
+	mounted() {
+
+	},
+	methods: {
+		
+	}
+}
+</script>
+
+<template>
+	<PageHeader/>
+  <PageMain/>
+  <pageFooter/>
+</template>
+
+
+<style lang="scss">
+// importo il foglio di stile generale dell'applicazione, non-scoped
+@use './styles/general.scss';
+</style>
+
+<style scoped lang="scss">
+// importo variabili
+@use './styles/partials/variables' as *;
+
+// ...qui eventuale SCSS di App.vue
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,31 @@
+<script>
+import { store } from "../store.js"
+
+export default {
+    name: "PageHeader",
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Sono un componente header</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+// importo variabili
+@use '../styles/partials/variables' as *;
+
+// ...qui eventuale SCSS 
+</style>
