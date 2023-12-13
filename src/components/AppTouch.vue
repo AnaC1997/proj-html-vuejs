b649a1051c549ab42eb22f29a65107ae5c535227
Ana Maria Correa <111539281+AnaC1997@users.noreply.github.com>
2023-12-13 21:31:03 +0100

    add style componente touch

--- src/components/AppTouch.vue
+++ src/components/AppTouch.vue
@@ -21,38 +21,39 @@ export default {
 <template>
     <div class=" container d-flex">
         <div class="col-6 p-4">
-            <h5 class="green">SEND IN TOUCH</h5>
-            <h1>Excellence in <span class="green"> Services</span> </h1>
+            <h5 class="green bolder">SEND IN TOUCH</h5>
+            <h1 class="bolder">Excellence in <span class="green"> Services</span> </h1>
             <P class="grey">We will respond to your message as soon as possible.</P>
             <div class="row g-3 grey">
                 <div class="col-6 ">
-                    <input type="text" class="form-control bGrey " placeholder="Name" aria-label="First name">
+                    <input type="text" class="form-control bGrey" placeholder="Name" aria-label="First name">
                 </div>
                 <div class="col-6 ">
                     <input type="text" class="form-control bGrey" placeholder="Email" aria-label="Email">
                 </div>
                 <div class="col-6">
-                    <input type="number" class="form-control bGrey" placeholder="Phone" aria-label="Number">
+                    <input type="number" class="form-control bGrey grey" placeholder="Phone" aria-label="Number">
                 </div>
 
                 <div class="col-6 ">
-                    <select class="form-select bGrey" aria-label="Default select example">
+                    <select class="form-select bGrey grey" aria-label="Default select example">
                         <option selected>-- Please choose an option --</option>
                         <option value="1">One</option>
                         <option value="2">Two</option>
                         <option value="3">Three</option>
                     </select>
                 </div>
-
-                <div class="col-12 ">
-                    <input type="text" style="height: 100px" class="form-control bGrey" placeholder="Email"
+                <div class="col-12">
+                    <input type="text" style="height: 100px" class="form-control bGrey grey" placeholder="Email"
                         aria-label="Last name">
                 </div>
+                <button class="btn btn-primary">SEND</button>
+
             </div>
         </div>
 
         <div class="col-6 p-4">
-            <h5>Example Inc.</h5>
+            <h5 class="bolder">Example Inc.</h5>
             <p class="grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             <span class="bGreen">
                 <p><i class="fas fa-phone"></i> {{ infoContact.telefono }}</p>
@@ -60,38 +61,52 @@ export default {
                 <p><i class="fas fa-map-marker"></i> {{ infoContact.indirizzo1 }}</p>
                 <p><i class="fas fa-map-marker"></i> {{ infoContact.indirizzo2 }}</p>
             </span>
-            <button class="btn btn-primary bGreen">VIEW MAP</button>
+            <button class="btn btn-primary">VIEW MAP</button>
         </div>
 
 
     </div>
-
-
-   
 </template>
+
+
 
 <style scoped lang="scss">
 // importo variabili
 @use '../styles/partials/variables' as *;
 
-.green{
+h1{
+    font-size: 2rem;
+    
+}
+
+.bolder{
+    font-weight: bolder;
+}
+
+.green {
     color: #058283;
 }
 
-.fas{
+.fas {
     color: #058283;
 }
 
-.bGreen{
+.bGreen {
     color: #058283;
+
 
 }
 
-.bGrey{
+.bGrey {
     background-color: #ECECEC;
 }
 
-.grey{
-   color: #828D94;
+.grey {
+    color: #828D94;
+}
+
+button{
+    color: white;
+    width: auto;
 }
 </style>
